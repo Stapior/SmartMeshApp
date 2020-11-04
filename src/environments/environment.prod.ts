38540633a1e63d20fddf6a929139d65a6159df55
pstachowiak <pstachowiak@consdata.pl>
2020-11-04 23:44:24 +0100

    Dodanie okna obiektów

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBpl8g_D7jNRhuR-TlX5JDP4Ix1AvM_8p4',
+    authDomain: 'meshhome-765cd.firebaseapp.com',
+    databaseURL: 'https://meshhome-765cd.firebaseio.com',
+    projectId: 'meshhome-765cd',
+    storageBucket: 'meshhome-765cd.appspot.com',
+    messagingSenderId: '943460618626',
+    appId: '1:943460618626:web:1366084e75824395ec7f9b',
+    measurementId: 'G-ZLC9D7HJ13'
+  }
 };
